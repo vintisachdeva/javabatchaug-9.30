0b3d4bf2be9d9748122df9e92d80079b0f2e7bb5
vintisachdeva <vintisachdeva17@gmail.com>
2017-08-02 11:19:29 +0530

    Add files via upload

---
+++ 1 Aug- Intro/firstprogram.java
@@ -0,0 +1,24 @@
+
+class hello{
+
+	//public-->package-centric language accessible to 	//anywhere
+	
+	//static--> no need to create object for main
+	//void--> no return type
+	//main--> name of my method
+
+	//String--> Predefined class--> datatype
+
+	//String args[]--> 
+
+	public static void main(String args[])
+	{
+	//System--> predefined class
+	//out--> out is an object PrintStream
+	//println--> method
+		System.out.println("Hello java");
+
+	}	
+
+
+}
