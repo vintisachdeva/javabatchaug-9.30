9f6b88461df073de51f011e175b64608ef5621d1
vintisachdeva <vintisachdeva17@gmail.com>
2017-08-02 11:23:03 +0530

    Add files via upload

---
+++ 2 Aug- Java Overview/Demo.java
@@ -0,0 +1,30 @@
+class Demo{
+	//java--> case sensitive -->
+	//java--> package centric language
+	//int value;
+	//int Value;
+
+	//public-->
+
+//static-->  no need to create an object for main
+//void--> no return type(no need to return something)
+//main--> method name
+//String args[]
+//String --> Predefined class as a datatype
+	
+	public static void main(int a[])
+	{
+
+	}
+	//java 1.5 version
+	static public void main(String... a)
+	{
+
+	}
+
+}
+
+
+
+
+ 
