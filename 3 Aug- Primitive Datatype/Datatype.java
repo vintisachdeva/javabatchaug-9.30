c690568a16eb0bf54421916f95273bd43fca9927
vintisachdeva <vintisachdeva17@gmail.com>
2017-08-03 21:06:16 +0530

    Add files via upload

---
+++ 3 Aug- Primitive Datatype/Datatype.java
@@ -0,0 +1,14 @@
+class Datatype{
+
+
+	public static void main(String args[])
+	{
+
+		//explicit typecast--> forcefully
+		byte value = (byte)256;	// 127 + 1 + 1(sign)
+		
+		//typecasting--> implicit
+		System.out.println(value);
+
+	}
+}
---
+++ 3 Aug- Primitive Datatype/PrimitiveType.java
@@ -0,0 +1,21 @@
+package com.bmpl.javabasic;
+
+public class PrimitiveType {
+
+	public static void main(String[] args) {
+		
+		//int value1 = 100;
+		//int value2 = 100;
+		
+		String value1 = new String("Hello");
+		String value2 = new String("Hello");
+		String value3 = value2;
+		
+		//out--> PrintStream class 
+		//print() --> data print
+		//ln--> new line
+		//== comparision operator --> compare data inside variable
+		System.out.println(value1 == value2);// 100 == 200--> true or false
+		
+	}
+}
